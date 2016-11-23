b94046fa2832896b3bf0c0fa87d7f23f278f7e3a
Daniele <iximiel@gmail.com>
2016-11-24 00:08:04 +0100

    tolto debug

--- wsgi.py
+++ wsgi.py
@@ -19,5 +19,5 @@ def webhook():
 if __name__ == '__main__':
 	ip = OPENSHIFT_PYTHON_IP
 	port = int(OPENSHIFT_PYTHON_PORT)
-	app.run(debug=True, ip = ip, port = port)
+	app.run(ip = ip, port = port)
 	
