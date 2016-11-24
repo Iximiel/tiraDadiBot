09d95b8a49dae99d1e64800d8be5bfb819088333
Daniele <iximiel@gmail.com>
2016-11-24 10:03:15 +0100

    aggiungo la pagina del webhook in wsgi non ho soluzioni migliori, purtroppo

--- myflaskBase.py
+++ myflaskBase.py
@@ -1,22 +1,11 @@
 from flask import Flask
-from settings import TELEGRAM_HOOK
-from telegram import Update
 
 app = Flask(__name__)
-'''
-from tiradadi import setBotUp
-update_queue, bot_instance = setBotUp()
-'''
 
 @app.route('/')
 def explanation():
-	return "<par>I'm creating this to make a Telegram bot for my friends<br>and I'm actually enjoing it(more or <b>less<\b>)!!</par>"
-	
-@app.route('/'+TELEGRAM_HOOK)
-def webhook():
-	if request.json:
-		update_queue.put(Update.de_json(request.json, bot_instance))
-	return ''
+	return "<par>I'm creating this to make a Telegram bot for my friends<br>and I'm actually enjoing it(more or <b>less</b>)!!</par>"
+
 	
 if __name__ == '__main__':
 	app.run()
--- wsgi.py
+++ wsgi.py
@@ -13,13 +13,19 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-'''
-#from myflaskBase import app as application
-#from myflaskBase import update_queue, bot_instance
+
+from myflaskBase import app as application
 from tiradadi import setBotUp
+from telegram import Update
+from settings import TELEGRAM_HOOK
 
 update_queue, bot_instance = setBotUp()
 
+@application.route('/'+TELEGRAM_HOOK)
+def webhook():
+	if request.json:
+		update_queue.put(Update.de_json(request.json, bot_instance))
+	return ''
 
 if __name__ == '__main__':
 	from wsgiref.simple_server import make_server
@@ -28,26 +34,3 @@ if __name__ == '__main__':
 	httpd.serve_forever()
 
 
-'''
-from flask import Flask
-from settings import TELEGRAM_HOOK
-from telegram import Update
-
-app = Flask(__name__)
-
-from tiradadi import setBotUp
-update_queue, bot_instance = setBotUp()
-
-
-@app.route('/')
-def explanation():
-	return "<par>I'm creating this to make a Telegram bot for my friends<br>and I'm actually enjoing it(more or <b>less<\b>)!!</par>"
-	
-@app.route('/'+TELEGRAM_HOOK)
-def webhook():
-	if request.json:
-		update_queue.put(Update.de_json(request.json, bot_instance))
-	return ''
-	
-if __name__ == '__main__':
-	app.run()
