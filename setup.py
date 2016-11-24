301882bd8c34e8e1711f8762387eea350487ec1e
Daniele <iximiel@gmail.com>
2016-11-24 08:37:27 +0100

    reboot del primo commit

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+#imposto i requisiti
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+
 setup(name='mydice',
 	version='0.1.0',
 	description='OpenShift App',
 	author='Daniele Rapetti',
 	author_email='iximie@gmail.com',
 	url='http://www.python.org/sigs/distutils-sig/',
-#	install_requires=['Django>=1.3'],
+	install_requires=requirements,
 	)
