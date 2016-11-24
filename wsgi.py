ffb0e305c6667d74b84f36f5cacc55ca739b5049
Daniele <iximiel@gmail.com>
2016-11-24 09:58:10 +0100

    provo a settare il server direttamente da flask

--- wsgi.py
+++ wsgi.py
@@ -13,9 +13,9 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-
-from myflaskBase import app as application
-from myflaskBase import update_queue, bot_instance
+'''
+#from myflaskBase import app as application
+#from myflaskBase import update_queue, bot_instance
 from tiradadi import setBotUp
 
 update_queue, bot_instance = setBotUp()
@@ -28,3 +28,26 @@ if __name__ == '__main__':
 	httpd.serve_forever()
 
 
+'''
+from flask import Flask
+from settings import TELEGRAM_HOOK
+from telegram import Update
+
+app = Flask(__name__)
+
+from tiradadi import setBotUp
+update_queue, bot_instance = setBotUp()
+
+
+@app.route('/')
+def explanation():
+	return "<par>I'm creating this to make a Telegram bot for my friends<br>and I'm actually enjoing it(more or <b>less<\b>)!!</par>"
+	
+@app.route('/'+TELEGRAM_HOOK)
+def webhook():
+	if request.json:
+		update_queue.put(Update.de_json(request.json, bot_instance))
+	return ''
+	
+if __name__ == '__main__':
+	app.run()
