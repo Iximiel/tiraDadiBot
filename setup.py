56086c153db1255f1561f31f3ef70fe9a673fdb6
Daniele <iximiel@gmail.com>
2016-11-24 08:18:05 +0100

    ritorno all'originale

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
-#imposto i requisiti
-with open('requirements.txt') as f:
-		requirements = f.read().splitlines()
-
-setup(name='tiraDadi',
-      version='0.1',
-      description='OpenShift App',
-      author='iximiel',
-      author_email='iximiel@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-	  install_requires=requirements,
-     )
+setup(name='mydice',
+	version='0.1.0',
+	description='OpenShift App',
+	author='Daniele Rapetti',
+	author_email='iximie@gmail.com',
+	url='http://www.python.org/sigs/distutils-sig/',
+#	install_requires=['Django>=1.3'],
+	)
